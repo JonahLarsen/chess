a9c2c4b0f5e2dc2d8cd62f07cf04e6b406603691
Jonah Larsen <Jonah.Larsen@gmail.com>
2024-03-20 22:19:51 -0600

    Added ServerFacade Class.

---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,15 @@
+package server;
+
+public class ServerFacade {
+
+  private final String serverURL;
+
+  public ServerFacade(String url) {
+    serverURL = url;
+  }
+
+  public void join() {
+
+  }
+
+}

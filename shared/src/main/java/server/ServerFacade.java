8ec5c21101acdbf7e63e44f9f1f4ab90147c2c83
Jonah Larsen <Jonah.Larsen@gmail.com>
2024-03-20 22:30:52 -0600

    Created Stubbed methods for ServerFacade.

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,5 +1,10 @@
 package server;
 
+import model.AuthData;
+import model.GameData;
+
+import java.util.Collection;
+
 public class ServerFacade {
 
   private final String serverURL;
@@ -8,8 +13,31 @@ public class ServerFacade {
     serverURL = url;
   }
 
-  public void join() {
+  public void joinGame() {
 
   }
 
+  public GameData createGame() {
+    return null;
+  }
+
+  public Collection<GameData> listGames() {
+    return null;
+  }
+
+  public void logout() {
+
+  }
+
+  public AuthData login() {
+    return null;
+  }
+
+  public void clear() {
+
+  }
+
+  public AuthData register() {
+    return null;
+  }
 }

66fbb0aa595665062dd6f70094d0d980c2cf2ac9
Jonah Larsen <Jonah.Larsen@gmail.com>
2024-03-22 13:41:05 -0600

    Added Stubbed methods for ChessClient and added eval method.

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,14 +1,91 @@
 package ui;
 
+import exception.ResponseException;
 import server.ServerFacade;
+
+import java.util.Arrays;
 
 public class ChessClient {
   private final ServerFacade server;
   private final String serverURL;
+  private State state = State.SIGNEDOUT;
 
+  public enum State {
+    SIGNEDOUT,
+    SIGNEDIN
+  }
 
   public ChessClient(String URL) {
     server = new ServerFacade(URL);
     serverURL = URL;
+  }
+
+
+  public String eval(String input) {
+    try {
+      var tokens = input.toLowerCase().split(" ");
+      var cmd = (tokens.length > 0) ? tokens[0] : "help";
+      var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+      return switch (cmd) {
+        case "quit" -> quit();
+        case "login" -> login();
+        case "register" -> register();
+        case "logout" -> logout();
+        case "createGame" -> createGame();
+        case "listGames" -> listGames();
+        case "joinGame" -> joinGame();
+        case "joinObserver" -> joinObserver();
+        default -> help();
+      };
+    } catch (ResponseException e) {
+      return e.getMessage();
+    }
+  }
+
+  public String quit() throws ResponseException {
+    return null;
+  }
+
+  public String login() throws ResponseException {
+    return null;
+  }
+
+  public String register() throws ResponseException {
+    return null;
+  }
+
+  public String logout() throws ResponseException {
+    assertSignedIn();
+    return null;
+  }
+
+  public String createGame() throws ResponseException {
+    assertSignedIn();
+    return null;
+  }
+
+  public String listGames() throws ResponseException {
+    assertSignedIn();
+    return null;
+  }
+
+  public String joinGame() throws ResponseException {
+    assertSignedIn();
+    return null;
+  }
+
+  public String joinObserver() throws ResponseException {
+    assertSignedIn();
+    return null;
+  }
+
+  public String help() {
+    return null;
+  }
+
+  private void assertSignedIn() throws ResponseException {
+    if (state == State.SIGNEDOUT) {
+      throw new ResponseException(400, "You must sign in");
+    }
   }
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,5 +1,10 @@
 package ui;
 
+import java.util.Scanner;
+
+import static ui.EscapeSequences.SET_BG_COLOR_BLUE;
+import static ui.EscapeSequences.SET_TEXT_COLOR_GREEN;
+
 public class Repl {
   private final ChessClient client;
 
@@ -10,5 +15,26 @@ public class Repl {
 
   public void run() {
     System.out.println("Welcome to 240 Chess. Type Help for a list of options.");
+
+    Scanner scanner = new Scanner(System.in);
+    var result = "";
+    while (!result.equals("quit")) {
+      printPrompt();
+      String line = scanner.nextLine();
+
+      try {
+        result = client.eval(line);
+        System.out.print(SET_BG_COLOR_BLUE + result);
+      } catch (Throwable e) {
+        var msg = e.toString();
+        System.out.print(msg);
+      }
+    }
+    System.out.println();
+  }
+
+
+  private void printPrompt() {
+    System.out.print("\n" + ">>> " + SET_TEXT_COLOR_GREEN);
   }
 }
